644a81c524a1ed6780a417f4ca8faf4f86658c92
oliverh32 <jiayuan.17@intl.zju.edu.cn>
2020-11-01 16:00:58 +0800

    adapt accepted papers

---
+++ pages/trans.py
@@ -0,0 +1,29 @@
+import xml
+from xml.dom.minidom import parse
+
+
+domTree = parse('paper.xml')
+root = domTree.documentElement
+rows = root.getElementsByTagName('Row')
+with open('tmp.txt','w') as f:
+    for i in range(len(rows)):
+        if i == 0 or i == 1:
+            continue
+        prefix1 = "<li><b><u>"
+        prefix2 = "</u></b> - <i>"
+        prefix3 = "</i></li>"
+
+        title = rows[i].getElementsByTagName('Data')[3].firstChild.data
+        author = rows[i].getElementsByTagName('Data')[7].firstChild.data
+        author_list = author.split(';')
+        author = ''
+        for j in range(len(author_list)):
+            author += author_list[j].split(' (')[0].split(' ')[-1] + ', '
+        author = author[:-2]
+
+        text = prefix1 + title + prefix2 + author + prefix3
+        f.write(text)
+
+
+
+
