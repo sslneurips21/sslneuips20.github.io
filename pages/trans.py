fe42ad91f94eea2546bac235f58fd6394cb64a53
oliverh32 <jiayuan.17@intl.zju.edu.cn>
2020-11-01 17:29:24 +0800

    adaptation of accepted paper

--- pages/trans.py
+++ pages/trans.py
@@ -5,7 +5,7 @@ from xml.dom.minidom import parse
 domTree = parse('paper.xml')
 root = domTree.documentElement
 rows = root.getElementsByTagName('Row')
-with open('tmp.txt','w') as f:
+with open('tmp.txt','w',encoding='utf-8') as f:
     for i in range(len(rows)):
         if i == 0 or i == 1:
             continue
@@ -14,12 +14,12 @@ with open('tmp.txt','w') as f:
         prefix3 = "</i></li>"
 
         title = rows[i].getElementsByTagName('Data')[3].firstChild.data
-        author = rows[i].getElementsByTagName('Data')[7].firstChild.data
-        author_list = author.split(';')
-        author = ''
-        for j in range(len(author_list)):
-            author += author_list[j].split(' (')[0].split(' ')[-1] + ', '
-        author = author[:-2]
+        author = rows[i].getElementsByTagName('Data')[7].firstChild.data.replace(';',',')
+        # author_list = author.split(';')
+        # author = ''
+        # for j in range(len(author_list)):
+        #     author += author_list[j].split(' (')[0].split(' ')[-1] + ', '
+        # author = author[:-2]
 
         text = prefix1 + title + prefix2 + author + prefix3
         f.write(text)
